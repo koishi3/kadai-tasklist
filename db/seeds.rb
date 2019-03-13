284eb6a3661bdced5e4db19ced47892ef60cc779
koishi <aldde.ue@gmail.com>
2019-03-13 12:53:09 +0000

    add test-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    def time_rand from = 0.0, to = Time.now
+        Time.at(from+rand*(to.to_f-from.to_f))
+    end
+    
+    def status_rand 
+        status_array=["未","進行中","済"]
+        status_array.sample
+    end
+    
+(1..50).each do |number|        
+    Task.create(title: 'test-title ' + number.to_s, content: 'test-content ' + number.to_s, deadline: time_rand, status: status_rand)
+end
